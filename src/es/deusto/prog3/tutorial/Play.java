a4c7972cb6a75e1a17470228732da9aef6a90ad6
ahrPope <ahrPope@169.254.43.132>
2017-10-26 09:22:50 +0200

    He hecho esto siguiendo un tutorial para aprender a utilizar la libreria slick

---
+++ src/es/deusto/prog3/tutorial/Play.java
@@ -0,0 +1,36 @@
+package es.deusto.prog3.tutorial;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Play extends BasicGameState{
+	
+	
+
+	public Play(int state) {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+		
+	}
+	
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		
+	}
+	
+	public int getID() {
+		return 1;
+	}
+
+	
+	
+
+}
